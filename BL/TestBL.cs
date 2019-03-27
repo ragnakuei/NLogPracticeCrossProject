3b1011080a17a50167aaf3072f037d16b5d51b1c
彭憲奎 <kuei_peng@mayohr.com>
2019-03-27 23:27:35 +0800

    加入專案檔案。

---
+++ BL/TestBL.cs
@@ -0,0 +1,25 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class TestBL
+    {
+        private readonly Logger _logger;
+
+        public TestBL()
+        {
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+
+        public void Test()
+        {
+            _logger.Debug(MethodBase.GetCurrentMethod());
+        }
+    }
+}
---
+++ NLogPracticeCrossProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL;
+
+namespace NLogPracticeCrossProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bl = new TestBL();
+            bl.Test();
+        }
+    }
+}
